8307212b61b0da28445a5492efc344614391dc40
Damon Crowley <z5425101@ad.unsw.edu.au>
2025-02-08 23:14:41 +1100

    starter pages

---
+++ frontend/src/Router.jsx
@@ -0,0 +1,43 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Landing from './pages/Landing'
+import Home from './pages/Home'
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />}>
+          <Route path="home" element={<Home />} />
+        </Route>
+        {/* <Route
+          path="/register"
+          element={
+            <Register
+              setTokenFn={setToken}
+              handleSuccess={handleNewToken}
+              showError={showErrorModal}
+            />
+          }
+        />
+        <Route
+          path="/preview-presentation/:id/:slideIndex"
+          element={<PreviewPresentation showError={showErrorModal} />}
+        /> */}
+      </Routes>
+      {/* <ErrorModal
+        show={showModal}
+        message={modalMessage}
+        onClose={closeModal}
+      /> */}
+    </>
+  )
+}
+
+export default App
