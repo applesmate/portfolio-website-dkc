93c3c759e52935a9f2e609ae05dac7f399d078e4
Damon Crowley <z5425101@ad.unsw.edu.au>
2025-02-16 19:04:33 +1100

    hide image on screen being too thin

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,23 @@
 import Header from "../components/Header"
 // import "../../public/fonts/BAUHS93.ttf"
+import { useState, useEffect } from "react";
 
 export default function Landing() {
+  const [showImages, setShowImages] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 868) {
+        setShowImages(false);
+      } else {
+        setShowImages(true);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     // <div id="aa">
     //   <svg xmlns="http://www.w3.org/2000/svg" style="position:absolute">
@@ -34,7 +50,7 @@ export default function Landing() {
       <div className="flex flex-col padding-0 margin-0 position-absolute top-0 align-center text-center">
         <Header/>
         <h1 className="text-white font-[Baumans] my-20">Damon Khor Crowley</h1>
-        <div className="relative w-2/8 h-150 text-center text-white self-center font-normal flex flex-col gap-6 justify-around">
+        <div className="relative w-2/8 min-w-100 h-150 text-center text-white self-center font-normal flex flex-col gap-6 justify-around">
           <h2 className="underline">Who am I?</h2>
           <h3>4th year Software Engineering student at UNSW</h3>
           <h3>Lead developer at TRIPLEBARREL</h3>
@@ -43,29 +59,33 @@ export default function Landing() {
           <h3>I am awesome as I am Damon Khor Crowley</h3>
         </div>
       </div>
-      <div className="absolute right-0 top-40">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 350" width="30vw" height={1000}>
-          <defs>
-            <clipPath id="mask1">
-              <polygon points="250,10 120,20 25,80 10,270 40,330 250,340"/>
-            </clipPath>
-          </defs>     
-      
-          <image id="image1" className="image__svg-image" width="100%" height="100%" clipPath="url(#mask1)" href="https://imagedelivery.net/SYqdpHruew-sMg33y-POLQ/8a106c8d-f891-4eb9-c3ec-50d50b75b500/public"/>
-          {/* <image id="image1" className="image__svg-image" width="100%" height="100%" clipPath="url(#mask1)" href="src/assets/linkedin.jpg"/> */}
-        </svg>
-      </div>
-      <div className="absolute left-0 top-40">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 350" width="30vw" height={1000}>
-          <defs>
-            <clipPath id="mask2">
-              <polygon points="0,10 250,120 200,340 0,310"/>
-            </clipPath>
-          </defs>     
-      
-          <image id="image2" className="image__svg-image" height="100%" x="-75%" clipPath="url(#mask2)" href="https://imagedelivery.net/SYqdpHruew-sMg33y-POLQ/1c07cb97-b077-4a27-280a-d8c777e96600/public"/>
-        </svg>
-      </div>
+      { showImages && 
+      <>
+        <div className="absolute right-0 top-40">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 350" width="30vw" height={1000}>
+            <defs>
+              <clipPath id="mask1">
+                <polygon points="250,10 120,20 25,80 10,270 40,330 250,340"/>
+              </clipPath>
+            </defs>     
+        
+            <image id="image1" className="image__svg-image" width="100%" height="100%" clipPath="url(#mask1)" href="https://imagedelivery.net/SYqdpHruew-sMg33y-POLQ/8a106c8d-f891-4eb9-c3ec-50d50b75b500/public"/>
+            {/* <image id="image1" className="image__svg-image" width="100%" height="100%" clipPath="url(#mask1)" href="src/assets/linkedin.jpg"/> */}
+          </svg>
+        </div>
+        <div className="absolute left-0 top-40">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 350" width="30vw" height={1000}>
+            <defs>
+              <clipPath id="mask2">
+                <polygon points="0,10 250,120 200,340 0,310"/>
+              </clipPath>
+            </defs>     
+        
+            <image id="image2" className="image__svg-image" height="100%" x="-75%" clipPath="url(#mask2)" href="https://imagedelivery.net/SYqdpHruew-sMg33y-POLQ/1c07cb97-b077-4a27-280a-d8c777e96600/public"/>
+          </svg>
+        </div>
+      </>
+      }
     </div>
       
   )
