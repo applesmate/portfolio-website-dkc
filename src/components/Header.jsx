05be148ff05679c53e2238360de52b4a9660107f
Damon Crowley <z5425101@ad.unsw.edu.au>
2025-02-19 01:04:33 +1100

    removed flashing border on click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ export default function Header() {
   return (
     <div className="flex flex-col w-4/5 h-2/11 self-center justify-space-between">
       <div className="flex flex-row items-center justify-around h-30">
-        <button className="hover:drop-shadow-[0_0_8px_rgba(255,255,255,1)] duration-300 text-white font-bold rounded" onClick={() => navigate("/home")}>Home</button>
-        <button className="hover:drop-shadow-[0_0_8px_rgba(255,255,255,1)] duration-300 text-white font-bold rounded" onClick={() => navigate("/projects")}>Projects</button>
-        <button className="hover:drop-shadow-[0_0_8px_rgba(255,255,255,1)] duration-300 text-white font-bold rounded" onClick={() => navigate("/about")}>About</button>
+        <div className="headerButton hover:drop-shadow-[0_0_8px_white] focus:outline-none focus:border-none duration-300 text-white font-bold" onClick={() => navigate("/home")}>Home</div>
+        <div className="headerButton hover:drop-shadow-[0_0_8px_white] focus:outline-none focus:border-none duration-300 text-white font-bold" onClick={() => navigate("/projects")}>Projects</div>
+        <div className="headerButton hover:drop-shadow-[0_0_8px_white] focus:outline-none focus:border-none duration-300 text-white font-bold" onClick={() => navigate("/about")}>Abouts</div>
       </div>
       <line className="w-full h-0.5 bg-white"></line>
     </div>
